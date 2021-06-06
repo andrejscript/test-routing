b4ddb36ef7100090f5fa650bb67b5d31fd122415
AndreWeb <andzion105@gmail.com>
2021-06-06 23:13:12 +0300

    create Routing test app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route, NavLink, Redirect } from 'react-router-dom';
+import './App.scss';
+import About from './About/About';
+import Cars from './Cars/Cars';
+import Contacts from './Contacts/Contacts';
+import CarDetail from './Cars/CarDetail/CarDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className='nav'>
+          <ul>
+            <li>
+              <NavLink to='/' exact>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={{
+                  pathname: '/about',
+                  search: '?sd',
+                  hash: '#d',
+                }}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/cars'>Cars</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contacts'>Contacts</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Switch>
+          <Route
+            path='/'
+            exact
+            render={() => <h1 style={{ textAlign: 'center' }}>Home</h1>}
+          />
+          <Route path='/cars' component={Cars} />
+          <Route path='/cars/:name' exact component={CarDetail} />
+          <Route path='/about' render={About} />
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+          {/* <Route render={() => <h1>404 Not Found </h1>} /> */}
+          <Redirect to={'/'} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Contacts/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Contacts.scss';
+
+const Contacts = props => (
+  <div className={'Contacts'}>
+    <h1>Contacts</h1>
+
+    <p>tel:034896770</p>
+    <p>adr:LA, Hoowers st.</p>
+  </div>
+);
+
+export default Contacts;
